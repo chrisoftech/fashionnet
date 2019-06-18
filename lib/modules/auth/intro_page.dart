3ea85a752648510ec431ef6041ea1351c5b21d98
chrisoftech <chrisoftech@gmail.com>
2019-06-18 21:13:31 +0000

    Re-structured the controls of intro-page.dart

--- lib/modules/auth/intro_page.dart
+++ lib/modules/auth/intro_page.dart
@@ -1,6 +1,32 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  TapGestureRecognizer _tapGestureRecognizer;
+
+  @override
+  void initState() {
+    super.initState();
+    _tapGestureRecognizer = TapGestureRecognizer()..onTap = _handleTap;
+  }
+
+  @override
+  void dispose() {
+    _tapGestureRecognizer.dispose();
+    super.dispose();
+  }
+
+  void _handleTap() {
+    HapticFeedback.vibrate();
+    print('sign-up button tapped');
+  }
+
   @override
   Widget build(BuildContext context) {
     final double _deviceHeight = MediaQuery.of(context).size.height;
@@ -44,6 +70,7 @@ class IntroPage extends StatelessWidget {
                       // flex: 2,
                       child: Container(
                         child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             Text(
                               'FASHIONet',
@@ -85,41 +112,8 @@ class IntroPage extends StatelessWidget {
                                 Expanded(
                                   child: Material(
                                     elevation: 5.0,
-                                    color: Colors.red,
-                                    borderRadius: BorderRadius.circular(30.0),
-                                    child: InkWell(
-                                      borderRadius: BorderRadius.circular(30.0),
-                                      onTap: () {
-                                        Navigator.of(context)
-                                            .pushReplacementNamed('/home');
-                                      },
-                                      child: Container(
-                                        height: 50.0,
-                                        alignment: Alignment.center,
-                                        decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(30.0)),
-                                        child: Text(
-                                          'GMAIL',
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 20.0,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 20.0),
-                            Row(
-                              children: <Widget>[
-                                Expanded(
-                                  child: Material(
-                                    elevation: 5.0,
-                                    color: Colors.indigo,
+                                    // color: Theme.of(context).primaryColor,
+                                    color: Colors.transparent,
                                     borderRadius: BorderRadius.circular(30.0),
                                     child: InkWell(
                                       borderRadius: BorderRadius.circular(30.0),
@@ -131,15 +125,29 @@ class IntroPage extends StatelessWidget {
                                         height: 50.0,
                                         alignment: Alignment.center,
                                         decoration: BoxDecoration(
+                                            border: Border.all(
+                                                width: 2.0,
+                                                color: Colors.white70),
                                             borderRadius:
                                                 BorderRadius.circular(30.0)),
-                                        child: Text(
-                                          'FACEBOOK',
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 20.0,
-                                            fontWeight: FontWeight.bold,
-                                          ),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: <Widget>[
+                                            Text(
+                                              'LOGIN',
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20.0,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Icon(
+                                              Icons.arrow_right,
+                                              size: 30.0,
+                                              color: Colors.white,
+                                            )
+                                          ],
                                         ),
                                       ),
                                     ),
@@ -147,6 +155,55 @@ class IntroPage extends StatelessWidget {
                                 ),
                               ],
                             ),
+                            SizedBox(height: 20.0),
+                            RichText(
+                              text: TextSpan(
+                                text: 'Don\'t have an account yet? ',
+                                children: [
+                                  TextSpan(
+                                    text: 'SIGN UP',
+                                    style: TextStyle(
+                                      color: Theme.of(context).primaryColor,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    recognizer: _tapGestureRecognizer,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            // Row(
+                            //   children: <Widget>[
+                            //     Expanded(
+                            //       child: Material(
+                            //         elevation: 5.0,
+                            //         color: Colors.indigo,
+                            //         borderRadius: BorderRadius.circular(30.0),
+                            //         child: InkWell(
+                            //           borderRadius: BorderRadius.circular(30.0),
+                            //           onTap: () {
+                            //             // Navigator.of(context)
+                            //             //     .pushReplacementNamed('/home');
+                            //           },
+                            //           child: Container(
+                            //             height: 50.0,
+                            //             alignment: Alignment.center,
+                            //             decoration: BoxDecoration(
+                            //                 borderRadius:
+                            //                     BorderRadius.circular(30.0)),
+                            //             child: Text(
+                            //               'FACEBOOK',
+                            //               style: TextStyle(
+                            //                 color: Colors.white,
+                            //                 fontSize: 20.0,
+                            //                 fontWeight: FontWeight.bold,
+                            //               ),
+                            //             ),
+                            //           ),
+                            //         ),
+                            //       ),
+                            //     ),
+                            //   ],
+                            // ),
                           ],
                         ),
                       ),
